998d2bf6fff27f1f95de877e7da76eb56b8e6e59
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2024-03-06 15:26:39 +0530

    first program

---
+++ hellowordl/target/debug/deps/hellowordl-326dde8f23a1829d.d
@@ -0,0 +1,5 @@
+/mnt/621534f8-aeb2-4740-a0f7-9832a31e7ac4/LearningRust/hellowordl/target/debug/deps/hellowordl-326dde8f23a1829d: src/main.rs
+
+/mnt/621534f8-aeb2-4740-a0f7-9832a31e7ac4/LearningRust/hellowordl/target/debug/deps/hellowordl-326dde8f23a1829d.d: src/main.rs
+
+src/main.rs:
---
+++ hellowordl/target/debug/hellowordl.d
@@ -0,0 +1,1 @@
+/mnt/621534f8-aeb2-4740-a0f7-9832a31e7ac4/LearningRust/hellowordl/target/debug/hellowordl: /mnt/621534f8-aeb2-4740-a0f7-9832a31e7ac4/LearningRust/hellowordl/src/main.rs
